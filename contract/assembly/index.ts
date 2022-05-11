29f097da5a4fa543f721ce7f1bcf153e28516ade
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-05-11 02:11:53 +0100

    addfunds contract

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,3 +1,31 @@
-import { Context, logging, storage } from 'near-sdk-as'
+import { Context, logging, storage, PersistentMap } from 'near-sdk-as'
 
 
+const recipientList = new PersistentMap<string, string[]> ('RL')
+const totalSent = new PersistentMap<string, i32[]> ('TS')
+
+export function addFunds(recipient:string, amount:i32):void {
+    if(recipientList.contains(Context.sender)) {
+        let getList = recipientList.getSome(Context.sender)
+        let getTotals = totalSent.getSome(Context.sender)
+
+        logging.log("User exits within list, updating lists")
+
+        if (getList.includes(recipient)) {
+            let getIndex = getList.indexOf(recipient)
+            let oldTotal = getTotals[getIndex]
+            let newTotal = oldTotal + amount
+            getTotals[getIndex] = newTotal
+            totalSent.set(Context.sender, getTotals)
+        } else {
+            getList.push(recipient);
+            recipientList.set(Context.sender, getList);
+            getTotals.push(amount);
+            totalSent.set(Context.sender, getTotals)
+        }
+    } else {
+        logging.log("User doesn't exist within storage, adding new user")
+        recipientList.set(Context.sender, [recipient])
+        totalSent.set(Context.sender, [amount])
+    }
+}
---
+++ src/components/SendTokens.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+const SendTokens = () => {
+    
+    const [recipients, setRecipients] = useState([])
+    const [valuesSent, setValues] = useState([])
+    const [balance, setBalance] = useState(0)
+
+  return (
+    <div>SendTokens</div>
+  )
+}
+
+export default SendTokens
